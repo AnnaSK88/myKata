c5f7e5dd52613a2c3160c8dc570a0f1ba5f83246
Anna SK <anna.sapun89@gmail.com>
2021-02-23 13:16:54 +0100

    upd1

---
+++ coЗвездочкой.js
@@ -0,0 +1,10 @@
+// есть часы часы и минуты, возвратите разницу между часами и минутами
+// function isDifferent(clock) {
+//     return clock.split(":")
+
+// }
+// console.log(isDifferent(['23:59'])) //1
+
+//Move every letter in the provided string forward 10 letters through the alphabet.
+//If it goes past 'z', start again at 'a'.
+//Input will be a string with length > 0.
