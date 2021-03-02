fcb0ec34ad3fd4d7d0b65469109ece48284ec270
Anna SK <anna.sapun89@gmail.com>
2021-03-02 19:14:42 +0100

    nomatter

--- 01-03.js
+++ 01-03.js
@@ -62,3 +62,14 @@ function paperwork(n, m) {
 console.log(paperwork(5, 5)) //25
 console.log(paperwork(-5, 5)) //0
 console.log(paperwork(-5, -5)) //0
+
+//5
+//1
+//Make a function that returns the value multiplied by 50 and increased by 6. 
+//If the value entered is a string it should return "Error".
+function problem(x) {
+    return x = String() ? x = 'Error' : (x * 50 + 6)
+}
+
+console.log(problem(1)) //56
+console.log(problem('hei')) // nan
---
+++ 123456.js
@@ -0,0 +1,12 @@
+// есть часы часы и минуты, возвратите разницу между часами и минутами
+function isDifferent(clock) {
+    return clock.map
+
+}
+
+console.log(isDifferent(['5:40'])) //260
+console.log(isDifferent(['23:59'])) //1
+
+//Move every letter in the provided string forward 10 letters through the alphabet.
+//If it goes past 'z', start again at 'a'.
+//Input will be a string with length > 0.
